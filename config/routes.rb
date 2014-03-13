430034d99acce89a2178e9f568bc9b3f0561f35c
Sooraj TP <soorajtpnlr@gmail.com>
2014-03-13 18:02:03 +0530

    routes to projects

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Tracker::Application.routes.draw do
+  resources :projects
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
