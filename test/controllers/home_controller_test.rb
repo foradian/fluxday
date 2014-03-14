4a7cb2906498b2fde38e729d35d43c666293a8ce
Sooraj TP <soorajtpnlr@gmail.com>
2014-03-14 10:50:06 +0530

    devise auth

---
+++ test/controllers/home_controller_test.rb
@@ -0,0 +1,14 @@
+require 'test_helper'
+
+class HomeControllerTest < ActionController::TestCase
+  test "should get index" do
+    get :index
+    assert_response :success
+  end
+
+  test "should get dashboard" do
+    get :dashboard
+    assert_response :success
+  end
+
+end
