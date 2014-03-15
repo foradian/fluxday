e61fdcd6f8f4fc80d771a4c01058a11e011a67fe
Sooraj TP <soorajtpnlr@gmail.com>
2014-03-15 12:31:35 +0530

    removed duplicate of image in user migration

--- db/migrate/20140315065143_add_additional_fields_to_users.rb
+++ db/migrate/20140315065143_add_additional_fields_to_users.rb
@@ -2,7 +2,6 @@ class AddAdditionalFieldsToUsers < ActiveRecord::Migration
   def change
     add_column :users, :name, :string
     add_column :users, :nickname, :string
-    add_column :users, :image, :string
     add_column :users, :email, :string
     add_column :users, :employee_code, :string
     add_column :users, :joining_date, :datetime
