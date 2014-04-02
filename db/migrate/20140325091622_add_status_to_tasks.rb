00b595d8409b54f1a49b5ab532dc28c7473218c5
Sooraj TP <soorajtpnlr@gmail.com>
2014-04-02 16:19:41 +0530

    change of default value

--- db/migrate/20140325091622_add_status_to_tasks.rb
+++ db/migrate/20140325091622_add_status_to_tasks.rb
@@ -1,5 +1,5 @@
 class AddStatusToTasks < ActiveRecord::Migration
   def change
-    add_column :tasks, :status, :string, :default=>'pending'
+    add_column :tasks, :status, :string, :default=>'active'
   end
 end
