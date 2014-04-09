bf5517bfe22da825ecc85b7ded7f0455bce98dc1
Sooraj TP <soorajtpnlr@gmail.com>
2014-04-09 13:44:19 +0530

    role default to member

--- db/migrate/20140315065219_create_team_members.rb
+++ db/migrate/20140315065219_create_team_members.rb
@@ -3,7 +3,7 @@ class CreateTeamMembers < ActiveRecord::Migration
     create_table :team_members do |t|
       t.integer :team_id
       t.integer :user_id
-      t.string :role
+      t.string :role, :default=>'member'
       t.string :status,:default=>'active'
 
       t.timestamps
