6aee1e29327c91d2aa594eb9cf0ebeb989b345ca
Sooraj TP <soorajtpnlr@gmail.com>
2014-04-10 10:44:35 +0530

    login failure message

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -3,12 +3,12 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
     # You need to implement the method below in your model (e.g. app/models/user.rb)
     @user = User.find_for_google_oauth2(request.env["omniauth.auth"], current_user)
 
-    if @user.persisted? && @user.persisted?
+    if @user.present? && @user.persisted?
       flash[:notice] = I18n.t "devise.omniauth_callbacks.success", :kind => "Google"
       sign_in_and_redirect @user, :event => :authentication
     else
       session["devise.google_data"] = request.env["omniauth.auth"]
-      redirect_to new_user_registration_url
+      redirect_to new_user_session_path, :alert=> 'No user account is associated with this email'
     end
   end
 
