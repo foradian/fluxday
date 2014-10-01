3639beae165e0f6226fadfbed069d8d29cce869b
Sooraj TP <soorajtpnlr@gmail.com>
2014-10-01 16:17:58 +0530

    handle 4th quarter

--- lib/time_to_diff.rb
+++ lib/time_to_diff.rb
@@ -21,8 +21,12 @@ module DateExtenstions
   def to_quarters
     date = self
     qs = [[1, 2, 3], [4, 5, 6], [7, 8, 9], [10, 11, 12]] #qs - quarters
-    q = qs[(date.month - 1) / 3]                         #q  - quarter
-    ["#{date.year}-#{q[0]}-1".to_date,("#{date.year}-#{q[2]+1}-1".to_date - 1.days)]
+    q = qs[(date.month - 1) / 3] #q  - quarter
+    unless q == [10, 11, 12]
+      ["#{date.year}-#{q[0]}-1".to_date, ("#{date.year}-#{q[2]+1}-1".to_date - 1.days)]
+    else
+      ["#{date.year}-#{q[0]}-1".to_date, ("#{date.year}-#{12}-31".to_date)]
+    end
   end
 end
 
