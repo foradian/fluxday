dd166e4d748ad2d056cc5ff24ed47d1820b54ef6
Sooraj TP <soorajtpnlr@gmail.com>
2016-05-20 19:33:58 +0530

    wkhtmltopdf renamed to make architecture dependant

--- config/initializers/wicked_pdf.rb
+++ config/initializers/wicked_pdf.rb
@@ -1,3 +1,5 @@
+# place wkhtmltopdf executable in lib folder to generate pdf reports
 WickedPdf.config = {
-  :exe_path => "#{::Rails.root}/lib/wkhtmltopdf-amd64"
+  :exe_path => "#{::Rails.root}/lib/wkhtmltopdf"
 }
+
